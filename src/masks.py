8066fa26e2d25d79d4c67d167fcf1fe1ac4b9646
Andrei <ggread123.rezerv@gmail.com>
2025-02-08 23:03:03 +0700

    rewrite get_mask_card_number and get_mask_account

--- src/masks.py
+++ src/masks.py
@@ -1,30 +1,29 @@
-from typing import Any
-
-card_number = 1234560000001234
-account_number = "11111111111111115567"
-
-
-def get_mask_card_number(user_card_number: Any) -> str:
+def get_mask_card_number(user_card_number: str):
     """Маскировка номера карты прользователя"""
-    user_card_number = str(user_card_number)
-    split_masked_card_number: str = ""
-    if user_card_number.isdigit() and len(user_card_number) == 16:
-        masked_card_number = user_card_number[:6] + "*" * (len(user_card_number) - 10) + user_card_number[12:16]
-        for i in range(0, len(masked_card_number), 4):
-            split_masked_card_number += masked_card_number[i : i + 4] + " "
-            #split_masked_card_number = split_masked_card_number.strip()
-        return split_masked_card_number
-    return "Номер карты введен неверно!"
+    split_masked_card_number = ''
+    cnt = 0
+    for i in user_card_number:
+        if not i.isdigit():
+            split_masked_card_number += i
+        else:
+            if cnt % 4 == 0 and cnt != 0:
+                split_masked_card_number += " "
+            if 6 <= cnt <= 11:
+                split_masked_card_number += "*"
+            else:
+                split_masked_card_number += i
+            cnt += 1
+    return split_masked_card_number
 
 
-def get_mask_account(user_account_number: Any) -> str:
+def get_mask_account(user_account_number: str):
     """Маскировка номера аккаунта прользователя"""
-    user_account_number = str(user_account_number)
-    if user_account_number.isdigit() and len(user_account_number) == 20:
-        masked_account_number: str = "**" + user_account_number[-4:]
-        return masked_account_number
-    return "Номер аккаунта введен неверно!"
-
-
-print(get_mask_card_number(card_number))
-print(get_mask_account(account_number))
+    masked_account_number: str = ''
+    cnt: int = 0
+    for i in user_account_number:
+        if not i.isdigit() or cnt > 20:
+            masked_account_number += i
+        elif cnt == 20:
+            masked_account_number += '**'
+        cnt += 1
+    return masked_account_number
