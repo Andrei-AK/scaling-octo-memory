8b919237fe89f2887a003e4023641df7434f3dce
Andrei <ggread123.rezerv@gmail.com>
2025-02-08 23:36:48 +0700

    used linters

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
-def get_mask_card_number(user_card_number: str):
+def get_mask_card_number(user_card_number: str) -> str:
     """Маскировка номера карты прользователя"""
-    split_masked_card_number = ''
-    cnt = 0
+    split_masked_card_number: str = ""
+    cnt: int = 0
     for i in user_card_number:
         if not i.isdigit():
             split_masked_card_number += i
@@ -16,14 +16,14 @@ def get_mask_card_number(user_card_number: str):
     return split_masked_card_number
 
 
-def get_mask_account(user_account_number: str):
+def get_mask_account(user_account_number: str) -> str:
     """Маскировка номера аккаунта прользователя"""
-    masked_account_number: str = ''
+    masked_account_number: str = ""
     cnt: int = 0
     for i in user_account_number:
         if not i.isdigit() or cnt > 20:
             masked_account_number += i
         elif cnt == 20:
-            masked_account_number += '**'
+            masked_account_number += "**"
         cnt += 1
     return masked_account_number
