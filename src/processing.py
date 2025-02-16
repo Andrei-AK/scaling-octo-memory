da32a1763671a09f62f5b1d5a8f8edc443078065
Andrei <ggread123.rezerv@gmail.com>
2025-02-16 17:07:55 +0700

    add filter_by_state add sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def filter_by_state(list_for_sort: list, sort_key: str = "EXECUTED") -> list:
+    """Фильтруем словари из списка по значению sotr_key в ключе state"""
+    sorted_list = []
+    for i in list_for_sort:
+        if i["state"] == sort_key:
+            sorted_list.append(i)
+    return sorted_list
+
+
+def sort_by_date(list_for_sort: list, sort_direction: bool = True) -> list:
+    """Сортировка сипска по дате"""
+    sorted_list = sorted(list_for_sort, key=lambda element: element["date"], reverse=sort_direction)
+    return sorted_list
