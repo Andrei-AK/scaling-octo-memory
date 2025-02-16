d61bedb781399916584db3fe05ff92e28e9089a3
Andrei <ggread123.rezerv@gmail.com>
2025-02-16 18:39:05 +0700

    fix import

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
 import string
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 # user_information = "Visa Platinum 7000792289606361"
 user_information = "Счет 73654108430135874305"
