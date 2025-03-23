2cf937e8836ffa655e07fb0e3de1fdd8d54c7354
Andrei <ggread123.rezerv@gmail.com>
2025-03-23 22:50:58 +0700

    update amount_transaction (add try-except)

--- src/external_api.py
+++ src/external_api.py
@@ -10,23 +10,25 @@ headers = {"apikey": apikey}
 
 def amount_transaction(transaction):
     """Получение/конвертация суммы транзакции в RUB"""
-    if transaction:
-        if transaction["operationAmount"]["currency"]["code"] != "RUB":
-            response = requests.request(
-                "GET",
-                f"https://api.apilayer.com/exchangerates_data/convert?"
-                f"to={'RUB'}&"
-                f"from={transaction['operationAmount']['currency']['code']}&"
-                f"amount={transaction['operationAmount']['amount']}",
-                headers=headers,
-            )
-            print(response)
-            status_code = response.status_code
-            print(status_code, type(status_code))
-            if status_code == 200:
-                converted_transaction = response.json()
-                return float(converted_transaction["result"])
+    try:
+        if transaction:
+            if transaction["operationAmount"]["currency"]["code"] != "RUB":
+                response = requests.request(
+                    "GET",
+                    f"https://api.apilayer.com/exchangerates_data/convert?"
+                    f"to={'RUB'}&"
+                    f"from={transaction['operationAmount']['currency']['code']}&"
+                    f"amount={transaction['operationAmount']['amount']}",
+                    headers=headers,
+                )
+                if response.status_code == 200:
+                    converted_transaction = response.json()
+                    return float(converted_transaction["result"])
+                else:
+                    print(f"Status code: {response.status_code}")
+                    return 0.0
             else:
-                print(f"Status code: {response.status_code}")
-        else:
-            return float(transaction["operationAmount"]["amount"])
+                return float(transaction["operationAmount"]["amount"])
+    except ValueError:
+        print("Неправильное значение JSON")
+        return 0.0
