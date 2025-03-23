50cea55b9afef8022ba02f9e0fe964cb53893338
Andrei <ggread123.rezerv@gmail.com>
2025-03-23 22:54:08 +0700

    add try-except

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,21 @@
 import json
-import os
 
 
 def read_json():
     """Чтение файла формата json"""
-    file_path = "../data/operations.json"
-    if os.path.exists(file_path):
+    try:
+        file_path = "C:/Users/ggrea/Desktop/ST PY/pj/data/operations.json"
         with open(file_path, "r", encoding="UTF-8") as f:
             file_data = f.read()
             if file_data:
                 json_data = json.loads(file_data)
-                if type(json_data) is list:
-                    return json_data
-    return []
+                return json_data
+            else:
+                print("Файл не содержит список.")
+                return []
+    except FileNotFoundError:
+        print("Файл не найден.")
+        return []
+    except json.JSONDecodeError:
+        print("Невозможно декодировать (преобразовать) JSON-данные.")
+        return []
