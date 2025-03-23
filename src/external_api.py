c23fdd44d0b12e1836e0046225f57182c9a945b8
Andrei <ggread123.rezerv@gmail.com>
2025-03-23 18:17:19 +0700

    create src/extrnal_api.py add function amount_transaction

--- src/external_api.py
+++ src/external_api.py
@@ -30,16 +30,3 @@ def amount_transaction(transaction):
                 print(f"Status code: {response.status_code}")
         else:
             return float(transaction["operationAmount"]["amount"])
-
-
-transaction = {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560",
-}
-
-print(amount_transaction(transaction))
